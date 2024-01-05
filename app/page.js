cf02a7db7088f3b3f2f46d313a2c80d78455fd77
alexkham <alexkha@netvision.net.il>
2024-01-05 17:18:54 +0200

    .

--- app/page.js
+++ app/page.js
@@ -2,9 +2,11 @@
 import styles from './page.module.css'
 
 export default function Home() {
+  const port=process.env.NEXT_PUBLIC_PORT;
   return (
     <main className={styles.main}>
       <h2>Mathematics is Awesome</h2>
+      <div>{port}</div>
     </main>
   )
 }
--- next.config.js
+++ next.config.js
@@ -2,3 +2,35 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+
+
+// next.config.js
+// module.exports = {
+//     webpackDevMiddleware: (config) => {
+//       config.watchOptions = {
+//         poll: 1000, // Check for changes every 1 second (adjust as needed)
+//       };
+//       config.devServer = {
+//         // Specify the desired port (e.g., 3001)
+//         port: 3001,
+//       };
+//       return config;
+//     },
+//   };
+//   /**
+//  * @type {import('next').NextConfig}
+//  */
+// const nextConfig = {
+//     /* config options here */
+//     webpackDevMiddleware: (config) => {
+//         config.watchOptions = {
+//           poll: 1000, // Check for changes every 1 second (adjust as needed)
+//         };
+//         config.devServer = {
+//           // Specify the desired port (e.g., 3001)
+//           port: 3001,
+//         };
+//         return config;
+//       },}
+   
+//   module.exports = nextConfig
