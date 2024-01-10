a3bcc067410cd02ee0affe04932bfc090b414882
alexkham <alexkha@netvision.net.il>
2024-01-10 15:30:19 +0200

    no lorem

--- app/page.js
+++ app/page.js
@@ -86,19 +86,7 @@ export default function Home() {
      <br></br>
      <br></br>
      <br></br>
-      <div class="hero">
-  <div class="container">
-    <h1>Welcome to our website</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, repudiandae.</p>
-  </div>
-</div>
-
-<section class="container content">
-  <h2>content 1</h2>
-  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque, animi sunt! Libero optio, asperiores quis ipsa eum eveniet labore nihil expedita nostrum laborum dicta at deleniti sapiente atque assumenda quo corrupti exercitationem animi minus nemo vero soluta alias perspiciatis! Aspernatur, laborum consectetur repellat repellendus aliquam sed. Cupiditate accusantium porro at quae, maxime itaque accusamus! Ipsam tempore, illo similique officia neque asperiores eveniet omnis rem veritatis cumque quos amet placeat dignissimos quisquam nesciunt quas. Adipisci perspiciatis veniam, voluptatibus dolores doloremque quisquam qui possimus, vero ab dolorum temporibus soluta libero similique assumenda debitis dolorem ut perferendis incidunt nobis facere eaque. Enim, nemo?</p>
-  <h3>content 2</h3>
-  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque, animi sunt! Libero optio, asperiores quis ipsa eum eveniet labore nihil expedita nostrum laborum dicta at deleniti sapiente atque assumenda quo corrupti exercitationem animi minus nemo vero soluta alias perspiciatis! Aspernatur, laborum consectetur repellat repellendus aliquam sed. Cupiditate accusantium porro at quae, maxime itaque accusamus! Ipsam tempore, illo similique officia neque asperiores eveniet omnis rem veritatis cumque quos amet placeat dignissimos quisquam nesciunt quas. Adipisci perspiciatis veniam, voluptatibus dolores doloremque quisquam qui possimus, vero ab dolorum temporibus soluta libero similique assumenda debitis dolorem ut perferendis incidunt nobis facere eaque. Enim, nemo?</p>
-</section> 
+    
      <br></br>
      <br></br>
      <br></br>
