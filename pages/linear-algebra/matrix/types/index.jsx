66bacaaa3b3226bfc3334cb1c0f2e6b6367a74c1
alexkham <alexkha@netvision.net.il>
2025-02-11 00:47:41 +0200

    keys

--- pages/linear-algebra/matrix/types/index.jsx
+++ pages/linear-algebra/matrix/types/index.jsx
@@ -113,7 +113,8 @@ export default function MatrixTypesPage({
             title: `Identity Matrix`,
             content: [
                 `Matrix where $a_{ij} = 1$ if $i = j$ and $a_{ij} = 0$ if $i \\neq j$ $ \\begin{pmatrix} 1 & 0 & \\cdots & 0 \\\\ 0 & 1 & \\cdots & 0 \\\\ \\vdots & \\vdots & \\ddots & \\vdots \\\\ 0 & 0 & \\cdots & 1 \\end{pmatrix} $`,
-                <MyList data={identityRelationships}/>
+                <MyList data={identityRelationships}
+                key={1}/>
             ]
         },
         {
@@ -121,7 +122,8 @@ export default function MatrixTypesPage({
             title: `Zero Matrix`,
             content: [
                 `Matrix where $a_{ij} = 0$ for all $i,j$ $ \\begin{pmatrix} 0 & 0 & \\cdots & 0 \\\\ 0 & 0 & \\cdots & 0 \\\\ \\vdots & \\vdots & \\ddots & \\vdots \\\\ 0 & 0 & \\cdots & 0 \\end{pmatrix} $`,
-                <MyList data={zeroRelationships}/>
+                <MyList data={zeroRelationships}
+                key={2}/>
             ]
         },
         {
@@ -129,7 +131,8 @@ export default function MatrixTypesPage({
             title: `Scalar Matrix`,
             content: [
                 `Matrix where $a_{ij} = k$ if $i = j$ and $a_{ij} = 0$ if $i \\neq j$ $ \\begin{pmatrix} k & 0 & \\cdots & 0 \\\\ 0 & k & \\cdots & 0 \\\\ \\vdots & \\vdots & \\ddots & \\vdots \\\\ 0 & 0 & \\cdots & k \\end{pmatrix} $`,
-                <MyList  data={scalarRelationships}/>
+                <MyList  data={scalarRelationships}
+                key={3}/>
             ]
         },
         {
@@ -137,7 +140,8 @@ export default function MatrixTypesPage({
             title: `Diagonal Matrix`,
             content: [
                 `Matrix where $a_{ij} = 0$ for all $i \\neq j$ $ \\begin{pmatrix} d_1 & 0 & \\cdots & 0 \\\\ 0 & d_2 & \\cdots & 0 \\\\ \\vdots & \\vdots & \\ddots & \\vdots \\\\ 0 & 0 & \\cdots & d_n \\end{pmatrix} $`,
-                <MyList data={diagonalRelationships}/>
+                <MyList data={diagonalRelationships}
+                key={4}/>
             ]
         },
         {
@@ -145,7 +149,8 @@ export default function MatrixTypesPage({
             title: `Symmetric Matrix`,
             content: [
                 `Matrix where $a_{ij} = a_{ji}$ for all $i,j$ $ \\begin{pmatrix} a_{11} & a_{12} & \\cdots & a_{1n} \\\\ a_{12} & a_{22} & \\cdots & a_{2n} \\\\ \\vdots & \\vdots & \\ddots & \\vdots \\\\ a_{1n} & a_{2n} & \\cdots & a_{nn} \\end{pmatrix} $`,
-                <MyList data={symmetricRelationships}/>
+                <MyList data={symmetricRelationships}
+                key={5}/>
             ]
         },
         {
@@ -153,7 +158,8 @@ export default function MatrixTypesPage({
             title: `Skew-Symmetric Matrix`,
             content: [
                 `Matrix where $a_{ij} = -a_{ji}$ for all $i,j$ $ \\begin{pmatrix} 0 & a_{12} & \\cdots & a_{1n} \\\\ -a_{12} & 0 & \\cdots & a_{2n} \\\\ \\vdots & \\vdots & \\ddots & \\vdots \\\\ -a_{1n} & -a_{2n} & \\cdots & 0 \\end{pmatrix} $`,
-                <MyList data={skewSymmetricRelationships}/>
+                <MyList data={skewSymmetricRelationships}
+                key={6}/>
             ]
         },
         {
@@ -161,7 +167,8 @@ export default function MatrixTypesPage({
             title: `Upper Triangular Matrix`,
             content: [
                 `Matrix where $a_{ij} = 0$ for all $i > j$ $ \\begin{pmatrix} u_{11} & u_{12} & \\cdots & u_{1n} \\\\ 0 & u_{22} & \\cdots & u_{2n} \\\\ \\vdots & \\vdots & \\ddots & \\vdots \\\\ 0 & 0 & \\cdots & u_{nn} \\end{pmatrix} $`,
-                <MyList data={upperTriangularRelationships}/>
+                <MyList data={upperTriangularRelationships}
+                key={7}/>
             ]
         },
         {
@@ -169,7 +176,8 @@ export default function MatrixTypesPage({
             title: `Lower Triangular Matrix`,
             content: [
                 `Matrix where $a_{ij} = 0$ for all $i < j$ $ \\begin{pmatrix} l_{11} & 0 & \\cdots & 0 \\\\ l_{21} & l_{22} & \\cdots & 0 \\\\ \\vdots & \\vdots & \\ddots & \\vdots \\\\ l_{n1} & l_{n2} & \\cdots & l_{nn} \\end{pmatrix} $`,
-                <MyList data={lowerTriangularRelationships}/>
+                <MyList data={lowerTriangularRelationships}
+                key={8}/>
             ]
         }
     ]
