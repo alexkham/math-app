4854fccaad29fc1fce3629f70b70f6e861d60410
Alex Khalamsky <alexkha@netvision.net.il>
2026-02-02 16:44:04 +0200

    layout js

--- app/layout.js
+++ app/layout.js
@@ -1,41 +1,3 @@
-// import { Inter } from 'next/font/google'
-// import './globals.css'
-// import GoHomeButton from './components/GoHomeButton'
-// import GoBackButton from './components/GoBackButton'
-// import ScrollUpButton from './components/scroll-up-button/ScrollUpButton'
-// import MyNavbar from './components/nav-bar/MyNavbar';
-// import { mainMenuStructure } from './components/nav-bar2/mainMenu'
-// import GenericNavbar from './components/nav-bar2/GenericNavbar'
-// import Head from 'next/head'
-
-// const inter = Inter({ subsets: ['latin'] })
-
-// export const metadata = {
-//   title: 'Website for People Who Are Learning Mathematics',
-//   description: 'Online Resource for Math Knowledge and Information Concerning Different Branches of Mathematics',
-// }
-
-// export default function RootLayout({ children }) {
-//   return (
-//     <html lang="en">
-//       <Head>
-//         <meta name="robots" content="noindex,follow" />
-//       </Head>
-//       <body suppressHydrationWarning={true}>
-//         {/* <GenericNavbar /> */}
-//         {/* <MyNavbar></MyNavbar> */}
-//         {/* <nav className='nav-bar'>
-//           <GoHomeButton className={'fixed-button'}/>                     
-//           <GoBackButton className={'fixed-button'}/>
-//         </nav> */}
-//         <div>
-//           {children}
-//         </div>
-//         <ScrollUpButton></ScrollUpButton>
-//       </body>
-//     </html>
-//   )
-// }
 
 
 
@@ -77,7 +39,7 @@ export default function RootLayout({ children }) {
         </Script>
 
         {/* Meta tags */}
-        <meta name="robots" content="noindex,follow" />
+        {/* <meta name="robots" content="noindex,follow" /> */}
       </head>
       <body suppressHydrationWarning={true} className={inter.className}>
         {/* Optional navbars */}
