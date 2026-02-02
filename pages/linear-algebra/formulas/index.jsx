1c260071810b0ce7074aa476589c4cb6469672ea
Alex Khalamsky <alexkha@netvision.net.il>
2026-02-02 05:19:34 +0200

    robots.txt

--- pages/linear-algebra/formulas/index.jsx
+++ pages/linear-algebra/formulas/index.jsx
@@ -1,42 +1,4 @@
-// import React from 'react'
-// import linearAlgebraFormulasList from '@/app/api/db/formulas/linear-algebra/linearAlgebraFormulas'
-// import Breadcrumb from '@/app/components/breadcrumb/Breadcrumb'
-// import '../../pages.css'
-// import FormulaAccordionWrapper from '@/app/components/examples/FormulaAccordionWrapper'
-// import ScrollUpButton from '@/app/components/scroll-up-button/ScrollUpButton'
-// import GenericNavbar from '@/app/components/nav-bar2/GenericNavbar'
-// import OperaSidebar from '@/app/components/nav-bar/OperaSidebar'
-// import FormulasTOC from '@/app/components/examples/FormulaTOC'
 
-// export default function LinearAlgebraFormulasPage() {
-
-//   const keyWords=['linear algebra formulas','linear algebra equations']
-//   return (
-//     <>
-//     <GenericNavbar/>
-//     <br/>
-//     <br/>
-//     <br/>
-//     <br/>
-//     <OperaSidebar 
-//       side='right'
-//       topOffset='65px' 
-//       sidebarWidth='45px'
-//       panelWidth='300px'
-      
-//       iconColor='white'
-//       panelBackgroundColor='#f2f2f2'/> 
-//     <Breadcrumb/>
-//     <h1 className='title' style={{marginTop:'-20px',marginBottom:'-10px'}}>Linear Algebra Formulas</h1>
-//     <FormulasTOC data={linearAlgebraFormulasList} />
-//     <div style={{transform:'scale(0.95)'}}>
-//     <FormulaAccordionWrapper data={linearAlgebraFormulasList} groupByField={['category']}/>
-//     </div>
-//     <br/>
-//     <ScrollUpButton/>
-//     </>
-//   )
-// }
 
 
 import React from 'react'
@@ -140,7 +102,7 @@ export default function LinearAlgebraFormulasPage({
         />
       </Head>
 
-      <GenericNavbar/>
+      {/* <GenericNavbar/> */}
       <br/>
       <br/>
       <br/>
@@ -167,7 +129,7 @@ export default function LinearAlgebraFormulasPage({
           />
         </div>
         <br/>
-        <ScrollUpButton/>
+        {/* <ScrollUpButton/> */}
       </main>
     </>
   )
